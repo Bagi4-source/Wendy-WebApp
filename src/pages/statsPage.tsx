0743e4cfc6441b081de1796c38b0d4b12a134bd4
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2024-06-30 15:54:25 +0300

    fix: url

--- src/pages/statsPage.tsx
+++ src/pages/statsPage.tsx
@@ -1,6 +1,6 @@
 export const StatsPage = () => {
   return <section className={'flex flex-col gap-4 justify-start'}>
     <p className={'text-xl'}>Statistics</p>
-    <img src="/public/stats.png" alt="stats" />
+    <img src="/stats.png" alt="stats" />
   </section>;
 };
